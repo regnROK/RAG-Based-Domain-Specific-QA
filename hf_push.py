4a11da0e5e398ed6dd134b4e5a2cb7083dd76f63
Harsh Davra <94609048+regnROK@users.noreply.github.com>
2025-04-18 18:41:45 +0530

    Update hf_push.py

--- hf_push.py
+++ hf_push.py
@@ -4,6 +4,6 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="flan-t5-xl-finetuned-tatqa/final_model",
-    repo_id="anjalipatel03/flan-t5-xl_finance_QA",
+    repo_id="base/flan-t5-xl_finance_QA",
     repo_type="model",
 )
